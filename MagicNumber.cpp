58691593eea367302e827615ed496f2b9b4b4f42
Abenezer Tiglu <aintabeni@gmail.com>
2026-02-12 10:08:52 +0300

    Implement the loop to continue until the user's guess matches the magic number

--- MagicNumber.cpp
+++ MagicNumber.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
+    int guess;
+    int magic = 67;
+    for(guess = 0; guess!= magic; ){
+        cout << "Guess the magic number: \n";
+        cin >> guess;
+    }
+    cout << "66666666666777777777777776️⃣7️⃣7️⃣7️⃣7️⃣";
 }
